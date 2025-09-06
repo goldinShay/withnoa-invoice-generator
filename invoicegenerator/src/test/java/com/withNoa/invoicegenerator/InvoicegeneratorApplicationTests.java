d8365c8f026798725d6c6bdc3de37b74c15ef6f1
Shay Goldin <goldinshay@gmail.com>
2025-09-06 12:58:52 +0200

    Initial commit: Spring Boot 3.5 project setup

---
+++ invoicegenerator/src/test/java/com/withNoa/invoicegenerator/InvoicegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withNoa.invoicegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
